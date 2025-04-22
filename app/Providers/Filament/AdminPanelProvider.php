61d0152ffcedbec0dc917d2c3d8b1bade4fc0fa5
kaxpe-lee <fmartinez@work-art.net>
2025-04-22 17:12:40 +0200

    cportal 0.1.3-alpha

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogoHeight('55px')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#233054',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
